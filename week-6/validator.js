ff625d78867015a05feebc7e88835b616ca9e2a2
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-07 21:48:15 -0600

    backing up

--- week-6/validator.js
+++ week-6/validator.js
@@ -5,4 +5,37 @@ import { FloatField } from "./float-field";
 import { FloatMinField } from "./float-min-field";
 import { FloatMaxField } from "./float-max-field";
 
-let;
+class Validator {
+  validators = [];
+  messages = [];
+
+  constructor(name, field) {
+    this.name = name;
+    this.field = field;
+  }
+
+  addRequiredField() {
+    this.validators.push(new RequiredField(this.name, this.field));
+  }
+
+  addRequiredFloatField() {
+    this.validators.push(new FloatField(this.name, this.field));
+  }
+
+  addFloatMinField(min) {
+    this.validators.push(new FloatMinField(this.name, this.field, min));
+  }
+
+  addFloatMaxField(max) {
+    this.validators.push(new FloatMaxField(this.name, this.field, max));
+  }
+
+  validate() {
+    for (let i of this.validators) {
+      if (false) {
+        this.validators.push(getMessage(i));
+      }
+    }
+    return true;
+  }
+}
