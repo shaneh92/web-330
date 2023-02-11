33f2c28a29dc85912c2312ea6d4d134853d0ba85
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-11 10:03:54 -0600

    added comments at the top of the documents

--- week-6/float-field.js
+++ week-6/float-field.js
@@ -1,3 +1,16 @@
+/*
+=================================================================================================================
+; Title: Assignment 6.2 Future Value Calculator
+; Author: Shane Hingtgen
+; Bellevue University
+; Date: 02/1/23
+; Description: We created a calculator that calculates a total with interest of years
+; Work Cited:
+    Web 330 HTML, CSS, and JavaScript Requirements
+    Web 330 Assign 6
+=================================================================================================================
+*/
+
 "use-strict";
 
 export class FloatField {
