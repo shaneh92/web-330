8093b4bc6fb63dd6aec2c47d8e73ee4b3fdd3a6d
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-21 21:04:04 -0600

    backing up

---
+++ week-8/http-client.js
@@ -0,0 +1,15 @@
+export class HttpClient {
+  async get(url, params) {
+    params = "";
+
+    url = new URL(url);
+    params = new URLSearchParams(url.search);
+
+    // URLSearchParams = url.search(params); //not sure about this step 2.	Instantiate a new URLSearchParams object, supplying it the params parameter and assigning it to the url.search property.
+
+    const res = await fetch(url.toString(), {
+      method: "GET",
+    });
+    return res.json(); //   4.	Return the res object literal as JSON.
+  }
+}
