14459ab9b6912218632fcc46a8c91d9cea7d5915
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-23 21:24:31 -0600

    backing up, trying to fix syntaxerror

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,13 +1,16 @@
+/*
+=================================================================================================================
+
+=================================================================================================================
+*/
 export class HttpClient {
   async get(url, params = "") {
     url = new URL(url);
     params = new URLSearchParams(url.search);
 
-    // URLSearchParams = url.search(params); //not sure about this step 2.	Instantiate a new URLSearchParams object, supplying it the params parameter and assigning it to the url.search property.
-
     const res = await fetch(url.toString(), {
-      method: "GET",
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
     });
-    return res.json(); //   4.	Return the res object literal as JSON.
+    return res.json();
   }
 }
