0f0035e5a794aa3b20b71438ebdbc25f3d106f73
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-25 09:32:42 -0600

    backing up

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -3,13 +3,26 @@
 
 =================================================================================================================
 */
-export class HttpClient {
-  async get(url, params = "") {
-    let newUrl = new URL(url);
-    newUrl.search = new URLSearchParams(params);
+// export class HttpClient {
+//   async get(url, params = "") {
+//     let newUrl = new URL(url);
+//     newUrl.search = new URLSearchParams(params);
 
-    const res = await fetch(newUrl.toString(), {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
+//     const res = await fetch(newUrl.toString(), {
+//       method: "GET", // *GET, POST, PUT, DELETE, etc.
+//     });
+//     return res.json();
+//   }
+// }
+
+export default class HttpClient {
+  async get(url, params) {
+    this.params = "";
+    url = new URL(url);
+    url.search = new URLSearchParams(params);
+    //declared as const to keep the value
+    const res = await fetch(url.toString(), {
+      method: "GET",
     });
     return res.json();
   }
