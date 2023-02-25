ff127d4cfdb8b88c9ba96d4ab4a27b278138a875
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-25 13:04:26 -0600

    added a css loader

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,6 +1,21 @@
 /*
 =================================================================================================================
-
+; Title: Assignment 8.2 WhatABook Part 2
+; Author: Shane Hingtgen
+; Bellevue University
+; Date: 02/25/23
+; Description: In this assignment we learn to website data converted into json format
+; Work Cited:
+    Web 330 HTML, CSS, and JavaScript Requirements
+    Web 330 Assign 8
+    MDN Docs https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    MDN Docs https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+    MDN Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+    MDN Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+    W3 Schools https://www.w3schools.com/js/js_api_fetch.asp
+    W3 Schools https://www.w3schools.com/jsref/jsref_search.asp
+    W3 Schools https://www.w3schools.com/jsref/obj_event.asp
+    W3 Schools https://www.w3schools.com/howto/howto_css_loader.asp
 =================================================================================================================
 */
 //our HttPClient class exported
@@ -11,10 +26,10 @@ export class HttpClient {
     // assign the variable to the new URL
     url = new URL(url);
     url.search = new URLSearchParams(params);
-    //declared as const to keep the value
+    // await and fetching the url, converting it to string
     const res = await fetch(url.toString(), {
-      method: "GET",
+      method: "GET", // GETs the data
     });
-    return res.json();
+    return res.json(); //returns as JSON
   }
 }
